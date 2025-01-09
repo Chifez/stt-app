a526ed4461cd291231eef3cb87afdb19a8b37ea3
emmanuel <chifez1@gmail.com>
2025-01-09 17:15:36 +0100

    updated the url

--- app/auth/register/actions.ts
+++ app/auth/register/actions.ts
@@ -46,7 +46,7 @@ export async function register(prevState: any, formData: FormData) {
     };
   }
 
-  const response = await fetch(`${baseUrl}/api/auth/register`, {
+  const response = await fetch(`https://${baseUrl}/api/auth/register`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(validatedFields.data),
